f3050ab036b2342e321be32e4ad27d04e0e7b452
Rafael Esteban Arango Castro <112355874+EstebanAR2@users.noreply.github.com>
2026-04-23 22:12:53 +0000

    feat: preparo app para despliegue en Render

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ function sumar(a, b) {
 }
 
 const http = require('http');
+const PORT = process.env.PORT || 3000;
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.end(JSON.stringify({ mensaje: 'Hola Mundo DevOps', resultado: sumar(2, 3) }));
 });
 
-server.listen(3000, () => console.log('Servidor corriendo en puerto 3000'));
+server.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
 
 module.exports = { sumar };
