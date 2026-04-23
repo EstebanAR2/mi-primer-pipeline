4611f3f84e38e9a8b765470601f8c5932f91fa8a
Rafael Esteban Arango Castro <112355874+EstebanAR2@users.noreply.github.com>
2026-04-23 22:25:11 +0000

    feat: cambio mensaje a Hola Arquitectos DevOps

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({ mensaje: 'Hola Mundo DevOps', resultado: sumar(2, 3) }));
+  res.end(JSON.stringify({ mensaje: 'Hola Arquitectos DevOps', resultado: sumar(2, 3) }));
 });
 
 server.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
