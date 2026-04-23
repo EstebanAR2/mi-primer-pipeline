d83ebd915aca3142cd51206a63d64cae4a171e73
Rafael Esteban Arango Castro <112355874+EstebanAR2@users.noreply.github.com>
2026-04-23 22:11:24 +0000

    fix: corrijo estructura de app.js

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-return a + b;// Función principal: suma dos números
 function sumar(a, b) {
   return a + b;
 }
 
-// Servidor web simple
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'application/json' });
@@ -12,5 +10,4 @@ const server = http.createServer((req, res) => {
 
 server.listen(3000, () => console.log('Servidor corriendo en puerto 3000'));
 
-module.exports = { suma };
-
+module.exports = { sumar };
